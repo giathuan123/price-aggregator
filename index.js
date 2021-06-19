6e33af3082a81eb7d8780eaa46928e08d0a66e1f
Gia Thuan Vu <vugiathuan123@gmail.com>
2021-06-19 00:59:35 -0700

    Add laz price getter

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const priceGetter = require('./priceGetter.js');
+const { priceGetter } = require('./priceGetter.js');
 const read = require('./read.js');
 
 async function main(){
-  const pg = await new priceGetter.priceGetter('https://google.com');
-  await pg.getPrice();
-
+  const pg = await new priceGetter('https://www.lazada.sg/products/local-warranty-canon-pixma-e560-e560r-advanced-wireless-all-in-one-with-auto-duplex-printing-colour-inkjet-printer-e-560r-e-560-e-560-colour-printer-color-inkjet-printer-color-printer-ink-tank-printer-inktank-printer-i309128437-s560548854.html?spm=a2o42.searchlist.list.4.795d7db226LrCG&search=1&freeshipping=1');
+  const firstPrice = await pg.getPrice();
+  console.log(firstPrice);
+  await read.prompt("WAITING");
+  await priceGetter._browser.close();
 }
 (async ()=>{
   await main();
-  await priceGetter.browser.close();
   read.stdio.close();
 })();
--- priceGetter.js
+++ priceGetter.js
@@ -1,29 +1,57 @@
 const puppeteer = require('puppeteer');
-var browser;
 
-async function init(){
-    if(module.exports.browser == undefined){
-        module.exports.browser = await puppeteer.launch({headless: false});
-        browser = module.exports.browser;
-    }
-};
-
-class priceGetter {
+class priceGetter{
+    static _browser;
+    static _opts;
     constructor(url){
         return new Promise( async (resolve, reject) => {
-            await init();
-            this.page = await browser.newPage();
+            if(priceGetter._browser == undefined)
+                priceGetter._browser = await puppeteer.launch({headless: false});
+            this.page = await priceGetter._browser.newPage();
             this.url = url;
-            if(page == undefined)
+            this.price = -1;
+            if(this.page == undefined)
                 reject("Can't create newPage");
             resolve(this);
         });
     }
     async getPrice(){
-       await this.page.goto(this.url);
+        return new Promise(async (res, rej) =>{
+        await this.page.goto(this.url, {'waitUntil': 'load'});
+        if(this.url.includes('shopee')){
+            this.price = await this._getShopeePrice();
+        }else if (this.url.includes('lazada')){
+            this.price = await this._getLazadaPrice();
+        }else{
+            throw new Error("URL is wrong");
+        }
+        if(this.price == -1){
+            throw new Error("Can't get price~");
+        }
+        res(this.price);
+    });
+    }
+    async _getLazadaPrice(){
+        return new Promise(async (res)=>{
+            const title = await this.page.title();
+            if(title.includes('Sorry'))
+                throw new Error('~Captcha~');
+            this.price = await this.page.evaluate((price)=>{
+                parseFloat(pdpTrackingData.pdt_price.replace('$',''));
+            });
+            if(this.price == -1)
+                throw new Error('Can\'t get price from lazada');
+            res(this.price);
+        }
+        );
+    }
+    // todo shopee
+    async _getShopeePrice(){
+        return new Promise((res)=>{
+            res();
+        }
+        );
     }
 }
 
-module.exports = {
-    priceGetter
-}
+module.exports.priceGetter = priceGetter;
